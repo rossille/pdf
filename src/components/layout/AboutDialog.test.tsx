7817a5ba617433a8224ded4b8c1b86a05ea123e9
Samuel Rossille <samuel.rossille@gmail.com>
2025-04-17 23:10:18 +0200

    Remove tests

--- src/components/layout/AboutDialog.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect, vi } from 'vitest';
-
-// Let's skip this test suite for now due to Material UI icon issues
-describe.skip('AboutDialog', () => {
-  it('dummy test to keep vitest happy', () => {
-    expect(true).toBe(true);
-  });
-});
